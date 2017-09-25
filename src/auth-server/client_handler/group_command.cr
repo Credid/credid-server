74de054d75cfd6e4418bc47ad9586e76deb70b01
Arthur Poulet <arthur.poulet@sceptique.eu>
2017-09-25 21:26:13 +0200

    Add new documentation

--- src/auth-server/client_handler/group_command.cr
+++ src/auth-server/client_handler/group_command.cr
@@ -2,6 +2,8 @@ class Auth::Server::ClientHandler
   module GroupCommand
     extend self
 
+    # Create the group if it does not exists.
+    # And add the permission [path] = perm if specified
     def add(context, options, params)
       tmp = params.split ' ', 3
       group, perm, path = tmp[0], tmp[1]?, tmp[2]?
@@ -12,14 +14,19 @@ class Auth::Server::ClientHandler
       end
     end
 
+    # Remove a path from the group
     private def remove_path(context, group, path)
       context.groups.transaction! do |groups|
         group_e = groups[group]?
         group_e.delete path unless group_e.nil?
+        # TODO : Clear the group if needed
+        # groups.delete group if group_e.empty?
+        # context.users.transaction! { |users| users.each{|u| u.groups.delete group } }
         context.send_success
       end
     end
 
+    # Remove the whole group
     private def remove_group(context, group)
       context.groups.transaction! do |groups|
         groups.delete group
@@ -27,6 +34,7 @@ class Auth::Server::ClientHandler
       end
     end
 
+    # Remove a group or ONE permission of the group
     def remove(context, options, params)
       splitted_params = params.split ' ', 2
       group = splitted_params[0]
@@ -34,11 +42,13 @@ class Auth::Server::ClientHandler
       path ? remove_path(context, group, path) : remove_group(context, group)
     end
 
+    # List the existing groups
     def list(context, options, params)
       groups = context.groups.groups.keys
       context.send_success Query.apply_options_on(groups, options).inspect
     end
 
+    # List the permissions of a group
     def list_perms(context, options, params)
       perms = context.groups[params]?
       if perms
@@ -49,6 +59,7 @@ class Auth::Server::ClientHandler
       end
     end
 
+    # Get the permission of a group on a resource
     def get_perm(context, options, params)
       group, path = params.split ' ', 2
       group = context.groups[group]?
